91bd6116d41618855e48c5c6ca1d43672d2d8500
adn67 <alejandro_di_nardo@hotmail.com>
2024-10-27 15:52:03 -0300

    modified astro.config settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-/* export default defineConfig({
-  base: "https://github.com/adn67/masterchef",
-}); */
+
+export default defineConfig({
+  site: 'https://adn67.github.io',
+  base: '/masterchef',
+  output: 'static',
+});
