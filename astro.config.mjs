d80acdcc50d0d6930870a88a17e687bad4a1350f
adn67 <alejandro_di_nardo@hotmail.com>
2024-10-27 13:16:00 -0300

    commented lines in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
+/* export default defineConfig({
   base: "https://github.com/adn67/masterchef",
-});
+}); */
