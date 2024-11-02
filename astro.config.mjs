b077838468201b5b35c0fc6ed963aeed721df6fb
adn67 <alejandro_di_nardo@hotmail.com>
2024-11-02 16:47:59 -0300

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,3 @@
-/* import { defineConfig } from 'astro/config';
-import dotenv from 'dotenv';
-
-// Cargar variables de entorno
-dotenv.config();
-
-export default defineConfig({
-  site: process.env.ASTRO_SITE_URL || 'http://localhost:4321',
-  base: process.env.ASTRO_SITE_URL.includes('github.io') ? '/masterchef' : '/',
-  output: 'static',
-}); */
-
-
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
 
@@ -18,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const siteUrl = process.env.ASTRO_SITE_URL || 'http://localhost:4321';
-const baseUrl = siteUrl.includes('github.io') ? '/masterchef' : '/';
+const baseUrl = siteUrl === 'http://localhost:4321' ? '/' : '/masterchef';
 
 export default defineConfig({
   site: siteUrl,
